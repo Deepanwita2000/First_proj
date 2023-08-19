089df74f2f101160ba680703f6fdd6c117fcdbaa
Deepanwita2000 <diyasarkar0104@gmail.com>
2023-08-19 21:45:11 +0530

    changes in calculations

--- Cal.java
+++ Cal.java
@@ -1,7 +1,9 @@
 public class Cal {
     public static void main(String[] args) {
-        int a = 40, b = 6 , c=10;
-        System.out.println(a+b+c);
+        int a = 40, b = 6, c = 10, d = 5;
+        int sum = 0;
+        sum = (a - b) + (c - d);
+        System.out.println(sum);
 
     }
 }
