cc0dcc8a82faaeadbc4706c2cf3cf4059532fe39
Deepanwita2000 <85988268+Deepanwita2000@users.noreply.github.com>
2023-08-19 21:39:34 +0530

    added another number

--- Cal.java
+++ Cal.java
@@ -1,7 +1,7 @@
 public class Cal {
     public static void main(String[] args) {
-        int a = 40, b = 6;
-        System.out.println(a + b);
+        int a = 40, b = 6 , c=10;
+        System.out.println(a+b+c);
 
     }
 }
