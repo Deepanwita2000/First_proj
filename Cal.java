8a472939192a503df42f51167499d5166a515528
Deepanwita2000 <85988268+Deepanwita2000@users.noreply.github.com>
2023-08-19 21:48:00 +0530

    changed the operators

--- Cal.java
+++ Cal.java
@@ -2,7 +2,7 @@ public class Cal {
     public static void main(String[] args) {
         int a = 40, b = 6, c = 10, d = 5;
         int sum = 0;
-        sum = (a - b) + (c - d);
+        sum = (a + b) - (c + d);
         System.out.println(sum);
 
     }
